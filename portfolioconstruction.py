a39f8214eb4843ff4c40696bf1697e28c542b011
Andrew Chan <70087509+chandr3w@users.noreply.github.com>
2025-03-25 18:50:12 -0700

    Update portfolioconstruction.py

--- portfolioconstruction.py
+++ portfolioconstruction.py
@@ -11,9 +11,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # App Title
+st.set_page_config(page_title="Atas VC Fund Simulator", page_icon="💼")
 st.markdown('<a href="https://atas.vc/"><img src="https://atas.vc/img/logo.png" width="150"></a>', unsafe_allow_html=True)
 st.title('Venture Capital Fund Simulator')
-st.set_page_config(page_title="Atas VC Fund Simulator", page_icon="💼")
 st.markdown(
     "This open source model was developed by [Andrew Chan](https://www.linkedin.com/in/chandr3w/) "
     "from [Atas VC](https://atas.vc/) as an open source resource to help push the venture capital into a future, data-driven era."
