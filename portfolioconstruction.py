3cfefb38cf7364e3aa973eb8da4f6b99dd4898ac
Andrew Chan <70087509+chandr3w@users.noreply.github.com>
2025-03-25 20:48:55 -0700

    Update portfolioconstruction.py

--- portfolioconstruction.py
+++ portfolioconstruction.py
@@ -13,7 +13,7 @@ import numpy_financial as npf
 
 
 # App Title
-st.set_page_config(page_title="Atas VC Fund Simulator")
+st.set_page_config(page_title="VC Fund Simulator", page_icon="https://atas.vc/img/favicon.png")
 st.image('https://atas.vc/img/logo.png', width=200)
 st.markdown(
     "This open source model was developed by [Andrew Chan](https://www.linkedin.com/in/chandr3w/), "
